5b1fc8b290f31879bde3ebc5c56b0cdba01c55e5
Gabriel Campos <gabrielcampos.dev@outlook.com>
2025-04-28 13:36:56 -0300

    alts

---
+++ frontend/svet-sistema/src/shared/components/input/input.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label!: string;
+  @Input() type: string = 'text';
+  @Input() placeholder: string = '';
+
+  value: string = '';
+  onChange = (value: any) => {};
+  onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {}
+}
